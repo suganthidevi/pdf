efbf336c57613e4e44839a065d09df702aff2a70
suganthidevi <33685971+suganthidevi@users.noreply.github.com>
2017-11-15 15:36:36 +0530

    pdf

---
+++ pdfcontent.php
@@ -0,0 +1,20 @@
+<?php
+
+ini_set('display_errors',true);
+ ini_set('error_reporting', E_ALL);
+
+?>
+
+<?php
+$source_pdf="Data/sample.pdf";
+$output_folder="";
+
+    if (!file_exists($output_folder)) { mkdir($output_folder, 0777, true);}
+passthru("pdftohtml $source_pdf $output_folder/new");
+// var_dump($a);
+$content= file_get_contents("Data/news.html");
+ $content = html_entity_decode($content);
+ $content = mb_convert_encoding($content, "HTML-ENTITIES", "UTF-8");
+ echo $content;
+
+?>
